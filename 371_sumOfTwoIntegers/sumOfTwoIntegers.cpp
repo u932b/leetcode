98a635c4b1e35ba69bbf83b0b9f0513f13a0fd70
za0 <za0@gmx.com>
2016-09-04 19:28:25 -0400

    Solution for 371

---
+++ 371_sumOfTwoIntegers/sumOfTwoIntegers.cpp
@@ -0,0 +1,42 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+int getSum(int &a, int &b){
+    int and_res = (a & b) << 1;
+    int xor_res = a ^ b;
+    while (and_res != 0){
+        int old_and_res = and_res;
+        and_res = (and_res & xor_res) << 1;
+        xor_res = old_and_res ^ xor_res;
+    }
+    return xor_res;
+
+}
+
+void testCase(){
+    vector<pair<int, int> > test_cases;
+    for (int i=0; i<test_cases.size(); i++){
+        int first = test_cases[i].first;
+        int second = test_cases[i].second;
+        printf("Number to be added: %d, %d \n", first, second);
+        int expected = first + second;
+        int result = getSum(first, second);
+        if (result != expected){
+            cout << "WRONG !!!! ";
+        }else{
+            cout << "GOOD ";
+        }
+        cout << "Expected: " << first + second << " Result of addition: " << getSum(first, second) << endl << endl;
+    }
+
+}
+int main(){
+    test_cases.push_back(make_pair(1,2));
+    test_cases.push_back(make_pair(10,2));
+    test_cases.push_back(make_pair(10,12));
+    test_cases.push_back(make_pair(10,999));
+    test_cases.push_back(make_pair(10,14));
+    testCase();
+    return 0;
+}
