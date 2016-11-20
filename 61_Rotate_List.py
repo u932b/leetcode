48c07293bdf9ce0c0aaf50559570d5c75fb499e4
u932b <curiojustus@gmx.com>
2016-11-20 08:53:53 -0500

    Build linkedlist from input.

--- 61_Rotate_List.py
+++ 61_Rotate_List.py
@@ -16,6 +16,7 @@ class Solution(object):
         tmp2 = head
         if not head:
             return head
+
         for i in xrange(k):
             head = head.next
         tmp = head.next
@@ -30,14 +31,43 @@ class Solution(object):
             tmp.next = tmp2
         return new_head
 
-if __name__ == "__main__":
-    ln3 = ListNode(3, None)
-    ln2 = ListNode(2, ln3)
-    ln1 = ListNode(1, ln2)
-    sol = Solution()
+    def traverseLinkedList(self, input_dic):
+        cur = input_dic['ln1']
+        while cur is not None:
+            print cur.val
+            cur = cur.next
 
-    # print sol.rotateRight(ln1, 1)
-    cur = ln1
-    while cur is not None:
-        print cur.val
-        cur = cur.next
+
+class Inputs(object):
+    def __init__(self, x, input_type):
+        self.input_data = x
+        self.input_type = input_type
+        self.input_result = None
+        if self.input_type == 'linkedlist':
+            self.toLinkedList(self.input_data)
+
+    def toLinkedList(self, input_data):
+        input_dic = {}
+        for i in reversed(input_data):
+            if i != len(input_data):
+                cur_node = 'ln%s' % i
+                nex_node = 'ln%s' % (i + 1)
+                input_dic[cur_node] = ListNode(i, input_dic[nex_node])
+            else:
+                cur_node = 'ln%s' % i
+                input_dic[cur_node] = ListNode(i, None)
+        self.input_result = input_dic
+
+    def returnResult(self):
+        return self.input_result
+
+
+if __name__ == "__main__":
+    input_data = [1, 2, 3, 4]
+    inp = Inputs(input_data, 'linkedlist')
+    input_dic = inp.returnResult()
+
+    sol = Solution()
+    sol.traverseLinkedList(input_dic)
+
+    print sol.rotateRight(input_dic['ln1'], 1)
